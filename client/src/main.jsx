4c380337a849e6d1767a4add7a3eb7d6bdc49886
winterpigeon <sahilsaksena1002@gmail.com>
2024-02-03 19:34:54 +0530

    feat
    - homepage changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { NextUIProvider } from '@nextui-org/react'
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import ContextState from "./components/Context/ContextState.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <React.StrictMode>
-    <NextUIProvider>
-    <main className="dark text-foreground bg-background">
-    <App />
-    </main>
-    </NextUIProvider>
+    <ContextState>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <App />
+        </main>
+      </NextUIProvider>
+    </ContextState>
   </React.StrictMode>
-)
+);
