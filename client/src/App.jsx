85294b4d58b49cae0c5b324aa3c860effbb0d9dc
avani <avanirocket@gmail.com>
2024-02-03 18:12:53 +0530

    card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import './App.css'
-import { Button } from "@nextui-org/react";
-
-function App(){
-  return(
-    <div>
+// App.jsx
+import React from 'react';
+import './App.css'; // Import a CSS file to style the components (create App.css in the same directory)
+import TransparentCard from './components/TransparentCard';
+function App() {
+  return (
+    <div className="app">
+     
+      <TransparentCard />
     </div>
   )
 }
+
 
 export default App
